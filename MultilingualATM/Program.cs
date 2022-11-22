c9dade23c5c7118758cea31214188833b3b15ce5
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-11-22 11:45:12 +0100

    upload

--- MultilingualATM/Program.cs
+++ MultilingualATM/Program.cs
@@ -83,6 +83,10 @@ public class Program
         public static decimal num;
         public static decimal num2;
         public static decimal num3;
+        public static decimal dept;
+        public static decimal dept2;
+        public static decimal dept3;
+
 
 
         public StartingMoney()
@@ -91,17 +95,17 @@ public class Program
         }
         public decimal First_Amount()
         {
-            decimal Total = TransferUser1() + _Amount[0];
+            decimal Total = TransferUser1() + _Amount[0] - DebitUser1();
             return Total;
         }
         public decimal Second_Amount()
         {
-            decimal Total = TransferUser2() + _Amount[1];
+            decimal Total = TransferUser2() + _Amount[1] - DebitUser2();
             return Total;
         }
         public decimal Third_Amount()
         {
-            decimal Total = TransferUser3() + _Amount[2];
+            decimal Total = TransferUser3() + _Amount[2] - DebitUser3();
             return Total;
         }
 
@@ -117,6 +121,19 @@ public class Program
         public static decimal TransferUser3()
         {
             return num3;
+        }
+        public static decimal DebitUser1()
+        {
+
+            return dept;
+        }
+        public static decimal DebitUser2()
+        {
+            return dept2;
+        }
+        public static decimal DebitUser3()
+        {
+            return dept3;
         }
     }
 
